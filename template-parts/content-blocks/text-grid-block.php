1811d4cbc8c9f93458afd6a3d954fdc2c425c403
Damien Doherty <damien@docandtee.com>
2026-03-10 14:42:43 +0000

    text grid block

--- template-parts/content-blocks/text-grid-block.php
+++ template-parts/content-blocks/text-grid-block.php
@@ -22,9 +22,9 @@
 						echo '
 							<div class="mt-8 mb-4 flex"><a 
 								href="'.esc_url($button_link).'" 
-								class="p-0 bg-transparent hover:bg-transparent text-white! hover:text-dark! flex items-center content-between !no-underline transition duration-200 ease-in-out text-nowrap"
+								class="arrow-btn p-0 bg-transparent hover:bg-transparent text-white! hover:text-dark! flex items-center content-between !no-underline transition duration-200 ease-in-out text-nowrap"
 							>'.esc_html($button_text).' 
-							<svg id="Group_14" data-name="Group 14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28.666" height="21.496" viewBox="0 0 28.666 21.496" class="btn-arrow ms-2">
+							<svg id="Group_14" data-name="Group 14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28.666" height="21.496" viewBox="0 0 28.666 21.496" class="arrow ms-2 transition duration-200 ease-in-out">
 								<defs>
 									<clipPath id="clip-path">
 									<rect id="Rectangle_42" data-name="Rectangle 42" width="28.666" height="21.496"/>
