7adc2a4811b3a8fe3664ccc3582daba869993bd9
Damien Doherty <damien@docandtee.com>
2026-03-10 14:39:13 +0000

    text grid block

--- template-parts/content-blocks/text-grid-block.php
+++ template-parts/content-blocks/text-grid-block.php
@@ -20,14 +20,14 @@
 					<?php if( $block_text ) { echo '<div class="copy-wrap">' .$block_text. '</div>'; } ?>
 					<?php if( $button_link && $button_text ) {
 						echo '
-							<div class="mt-8 mb-4"><a 
+							<div class="mt-8 mb-4 flex"><a 
 								href="'.esc_url($button_link).'" 
 								class="py-3 px-5 font-bold bg-primary hover:bg-dark border-primary text-white! rounded-full flex items-center content-between !no-underline transition duration-200 ease-in-out text-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:outline-none"
 							>'.esc_html($button_text).' 
-							<svg id="Group_14" data-name="Group 14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28.666" height="21.496" viewBox="0 0 28.666 21.496">
+							<svg id="Group_14" data-name="Group 14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28.666" height="21.496" viewBox="0 0 28.666 21.496" class="btn-arrow ms-2">
 								<defs>
 									<clipPath id="clip-path">
-									<rect id="Rectangle_42" data-name="Rectangle 42" width="28.666" height="21.496" fill="#faf4e7"/>
+									<rect id="Rectangle_42" data-name="Rectangle 42" width="28.666" height="21.496"/>
 									</clipPath>
 								</defs>
 								<g id="Group_13" data-name="Group 13" clip-path="url(#clip-path)">
