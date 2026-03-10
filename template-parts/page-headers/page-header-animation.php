c8f8159cedbe9bb9584d538d160984dc2ccb75d1
Damien Doherty <damien@docandtee.com>
2026-03-10 16:23:14 +0000

    particle animation

--- template-parts/page-headers/page-header-animation.php
+++ template-parts/page-headers/page-header-animation.php
@@ -3,8 +3,10 @@
 ?>
 <section class="page-header particles-bg relative overflow-hidden hero-height flex items-end justify-center" aria-label="animnated page header background">
 
-        <div class="particle particle-1 absolute w-full h-full"></div>
-        <div class="particle particle-1b absolute w-full h-full"></div>
+        <div class="particle particle-1 absolute w-full h-full opacity-60"></div>
+        <div class="particle particle-1b absolute w-full h-full opacity-60"></div>
+        <div class="particle particle-2 absolute w-full h-full opacity-40"></div>
+        <div class="particle particle-2b absolute w-full h-full opacity-40"></div>
 
 
     <div class="header-content relative w-full text-white">
