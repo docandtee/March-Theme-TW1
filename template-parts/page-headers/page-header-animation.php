9b59eda639bd796299a4cb7e83393537924aaba0
Damien Doherty <damien@docandtee.com>
2026-03-10 15:30:40 +0000

    page header animation

---
+++ template-parts/page-headers/page-header-animation.php
@@ -0,0 +1,17 @@
+<?php 
+    $photo_credit = get_field('photo_credit');
+?>
+<section class="page-header relative overflow-hidden hero-height flex items-end justify-center" aria-label="animnated page header background">
+
+    <div class="absolute flex justify-center items-center w-full h-full">
+        <div class="particle particle-1"></div>
+        <div class="particle particle-2"></div>
+        <div class="particle particle-3"></div>
+    </div>
+
+    <div class="header-content relative w-full text-white">
+        <?php get_template_part('template-parts/page-headers/page-header-title'); ?>
+    </div>
+    <?php if($photo_credit) { echo '<div class="photocredit absolute z-3 top-0 end-0 small p-3 text-white" role="note" aria-label="Photo credit">'.esc_html($photo_credit).'</div>';} ?>
+
+</section>
