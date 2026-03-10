2d0bb375d7dc88d8ff8103eb9853e60c68807d30
Damien Doherty <damien@docandtee.com>
2026-03-10 14:40:44 +0000

    text grid block

--- template-parts/content-blocks/text-grid-block.php
+++ template-parts/content-blocks/text-grid-block.php
@@ -22,7 +22,7 @@
 						echo '
 							<div class="mt-8 mb-4 flex"><a 
 								href="'.esc_url($button_link).'" 
-								class="py-3 px-5 font-bold bg-primary hover:bg-dark border-primary text-white! rounded-full flex items-center content-between !no-underline transition duration-200 ease-in-out text-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:outline-none"
+								class="p-0 font-bold bg-transparent hover:bg-transparent text-white hover:text-dark flex items-center content-between !no-underline transition duration-200 ease-in-out text-nowrap"
 							>'.esc_html($button_text).' 
 							<svg id="Group_14" data-name="Group 14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28.666" height="21.496" viewBox="0 0 28.666 21.496" class="btn-arrow ms-2">
 								<defs>
