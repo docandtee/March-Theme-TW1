7694ab7bd32b8778e7704b3fbe028149f40865c9
Damien Doherty <damien@docandtee.com>
2026-03-11 14:04:30 +0000

    testimonials slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -60,22 +60,6 @@ document.addEventListener( 'DOMContentLoaded', function () {
             speed: 700,
             interval: 4000,
             autoplay: true,
-            perPage: 3,
-            perMove: 1,
-            breakpoints: {
-                480: {
-                    perPage: 1,
-                },
-                782: {
-                    perPage: 1,
-                },
-                960: {
-                    perPage: 2,
-                },
-                1280: {
-                    perPage: 3,
-                },
-            },
         } ).mount();
     }
 
