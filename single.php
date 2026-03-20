e71261a5a76847ed3933437e909523cf9cdb9b81
Damien Doherty <damien@docandtee.com>
2026-03-20 16:18:26 +0000

    singles

--- single.php
+++ single.php
@@ -11,7 +11,17 @@ get_header();
 <div class="container my-8 mx-auto">
     <?php if (have_posts()): ?>
         <?php while (have_posts()): the_post(); ?>
-            <?php get_template_part('template-parts/content', 'single'); ?>
+            <?php
+            $post_type = get_post_type();
+
+                // Try to load a template like content-single-{post_type}.php first
+                if ( locate_template( "template-parts/content-single-{$post_type}.php" ) ) {
+                    get_template_part( 'template-parts/content-single', $post_type );
+                } else {
+                    // Fallback to the default single content template
+                    get_template_part( 'template-parts/content', 'single' );
+                }
+            ?>
         <?php endwhile; ?>
     <?php endif; ?>
 </div>
