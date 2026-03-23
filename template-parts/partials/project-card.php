914ad076bf07b4b1f198b81e25a566e98736c85a
Damien Doherty <damien@docandtee.com>
2026-03-23 12:51:12 +0000

    project card

--- template-parts/partials/project-card.php
+++ template-parts/partials/project-card.php
@@ -17,7 +17,7 @@
 		<?php endif; ?>
 		<div class="card-body flex flex-col justify-between grow">
 			<div class="card-wrapper">
-				<h3 class="mt-3! mb-1 mt-0 leading-6 text-lg">
+				<h3 class="mt-3! mb-1 mt-0 leading-6 text-xl">
 					<a href="<?php the_permalink(); ?>" class="no-underline! text-dark! hover:text-primary! transition duration-200" itemprop="url">
 						<?php the_title(); ?>
 					</a>
