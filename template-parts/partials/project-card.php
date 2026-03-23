bbf972c9dd53a5a5de697ef29c4886cc0ffc4f13
Damien Doherty <damien@docandtee.com>
2026-03-23 12:31:39 +0000

    post list and project card

--- template-parts/partials/project-card.php
+++ template-parts/partials/project-card.php
@@ -18,13 +18,25 @@
 		<div class="card-body flex flex-col justify-between grow">
 			<div class="card-wrapper">
 				<h4 class="my-3! mt-0 leading-6">
-					<a href="<?php the_permalink(); ?>" class="no-underline! hover:text-primary transition duration-200" itemprop="url">
+					<a href="<?php the_permalink(); ?>" class="no-underline! text-dark hover:text-primary transition duration-200" itemprop="url">
 						<?php the_title(); ?>
 					</a>
 				</h4>
 				<div class="post-excerpt text-sm">
 					<?php the_excerpt(); ?>
 				</div>
+				<a href="<?php the_permalink(); ?>" class="flex items-center justify-between text-dark hover: text-primary">Read more
+					<svg id="Group_14" data-name="Group 14" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="28.666" height="21.496" viewBox="0 0 28.666 21.496" class="arrow ms-2 transition duration-200 ease-in-out">
+						<defs>
+							<clipPath id="clip-path">
+							<rect id="Rectangle_42" data-name="Rectangle 42" width="28.666" height="21.496"/>
+							</clipPath>
+						</defs>
+						<g id="Group_13" data-name="Group 13" clip-path="url(#clip-path)">
+							<path id="Path_9" data-name="Path 9" d="M28.405,11.381a.9.9,0,0,0,0-1.265L18.55.26a.895.895,0,1,0-1.265,1.265l8.326,8.326H.9a.9.9,0,0,0,0,1.792H25.611L17.285,19.97a.895.895,0,0,0,1.265,1.265Z" transform="translate(0 0)"/>
+						</g>
+					</svg>
+				</a>
 			</div>
 		</div>
 	</div>
