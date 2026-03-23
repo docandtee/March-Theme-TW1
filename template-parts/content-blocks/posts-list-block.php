8e0931b684961e269bd9e4a2bae3a0b1f491b1f7
Damien Doherty <damien@docandtee.com>
2026-03-23 12:23:57 +0000

    post list and project card

--- template-parts/content-blocks/posts-list-block.php
+++ template-parts/content-blocks/posts-list-block.php
@@ -1,12 +1,11 @@
 <?php 
-	$fullwidth = get_field('full_width_blocks');
 	$section_header = get_field('section_header');
+	$section_copy = get_field('section_copy');
 	$post_type = get_field('post_type');
 	$post_category = get_field('post_category');
 	$number_of_posts_to_display = get_field('number_of_posts_to_display');
 	$display_order_by = get_field('display_order_by');
 	$display_order = get_field('display_order');
-	$remove_bottom_margin = get_field('remove_bottom_margin');
 
 	$args = array(
 		'post_type' => $post_type,
@@ -19,23 +18,19 @@
 	if ( $the_query->have_posts() ) : 
 ?>
 
-<section class="<?php if(!$remove_bottom_margin) { echo ' section-m-b '; } ?> <?php if( $fullwidth ) {echo ' fullwidth px-5 lg:px-10';} ?> overflow-hidden" aria-label="Posts list content">
-
-	<?php if($section_header) : ?>
+<section class="fullwidth overflow-hidden bg-light section-p-t section-p-b" aria-label="Posts list content">
+	<div class="container">
+		<?php if( $section_header ) { echo '<h2 class="mb-6">'.esc_html( $section_header ).'</h2>'; } ?>
+		<?php if( $section_copy ) { echo '<h2 class="mb-6">'.esc_html( $section_copy ).'</div>'; } ?>
+		<?php endif; ?>
 		<div class="grid grid-flow-row grid-cols-12 gap-8">
-			<header class="col-span-12 text-center">
-				<h2 class="section-m-b"><?php echo esc_html($section_header); ?></h2>
-			</header>
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+				<div class="col-span-12 md:col-span-6 lg:col-span-4 flex">
+					<?php get_template_part('template-parts/partials/project-card'); ?>
+				</div>
+			<?php endwhile; ?>
 		</div>
-	<?php endif; ?>
-	<div class="grid grid-flow-row grid-cols-12 gap-8">
-		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-			<div class="col-span-12 md:col-span-6 lg:col-span-4">
-				<?php get_template_part('template-parts/partials/hover-block'); ?>
-			</div>
-		<?php endwhile; ?>
 	</div>
-
 </section>
 
 <?php endif; wp_reset_postdata(); ?>
---
+++ template-parts/partials/project-card.php
@@ -0,0 +1,31 @@
+<div 
+	class="w-full h-full"
+	x-data="{ shown: false }" 
+	x-intersect.half="shown = true" 
+>
+	<div 
+		class="w-full h-full flex flex-col"
+		x-show="shown" 
+		x-transition:enter="transition ease-out duration-300"
+		x-transition:enter-start="opacity-0 scale-50"
+		x-transition:enter-end="opacity-100 scale-100"
+	>
+		<?php if (has_post_thumbnail()) : ?>
+			<a href="<?php the_permalink(); ?>" aria-label="<?php echo esc_attr( get_the_title() ); ?> - Featured image" itemprop="url" class="overflow-hidden rounded-lg relative">
+				<?php docandtee_responsive_image(null, null, '(min-width: 960px) 20vw, (min-width: 782px) 30vw, 80vw', 'aspect-4/3 w-full object-cover'); ?>
+			</a>
+		<?php endif; ?>
+		<div class="card-body flex flex-col justify-between grow">
+			<div class="card-wrapper">
+				<h4 class="my-3! mt-0 leading-6">
+					<a href="<?php the_permalink(); ?>" class="no-underline! hover:text-primary transition duration-200" itemprop="url">
+						<?php the_title(); ?>
+					</a>
+				</h4>
+				<div class="post-excerpt text-sm">
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
