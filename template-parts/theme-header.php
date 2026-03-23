815f4a2dfd0344eb382484740ce595209592d74f
Damien Doherty <damien@docandtee.com>
2026-03-23 10:22:32 +0000

    case studies header

--- template-parts/theme-header.php
+++ template-parts/theme-header.php
@@ -20,6 +20,7 @@
             if (
                 ! has_post_thumbnail( $post_id )
                 || ( is_single() && get_post_type() === 'post' )
+                || ( is_single() && get_post_type() === 'case-studies' )
                 || is_archive()
             ) { 
                 echo ' relative bg-dark '; 
