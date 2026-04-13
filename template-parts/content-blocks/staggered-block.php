f0a1e9e18e28b71fe744f654c8efce90e6c24472
Damien Doherty <damien@docandtee.com>
2026-04-13 15:04:23 +0100

    staggered boxes

--- template-parts/content-blocks/staggered-block.php
+++ template-parts/content-blocks/staggered-block.php
@@ -35,9 +35,9 @@
 				<?php if( $block_text ) { echo '<div class="copy-wrap">' .$block_text. '</div>'; } ?>
 				<?php if( $button_link && $button_text ) {
 					echo '
-						<div class="mt-8 mb-4"><a 
+						<div class="mt-12"><a 
 							href="'.esc_url($button_link).'" 
-							class="py-3 px-5 font-bold bg-primary hover:bg-dark border-primary text-white! rounded-full !no-underline transition duration-200 ease-in-out text-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:outline-none"
+							class="staggered-btn py-3 px-5 font-bold bg-primary hover:bg-dark border-primary text-white! rounded-full !no-underline transition duration-200 ease-in-out text-nowrap focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:outline-none"
 						>'.esc_html($button_text).'</a></div>
 					';
 				} ?>
