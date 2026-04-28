01dcbd522585048bc8fe03cdd3519a9e854541f2
Damien Doherty <damien@docandtee.com>
2026-04-28 12:54:31 +0100

    page header

--- page.php
+++ page.php
@@ -12,11 +12,13 @@ $pagewidth = get_field('page_width');
 
     <?php if (have_posts()): ?>
         <?php get_template_part('template-parts/page-headers/page-header'); ?>
-        <div class="container mx-auto <?php if( $pagewidth == 'narrow' ) { echo ' max-w-4xl '; } elseif( $pagewidth == 'fullwidth' ) { echo 'max-w-lvw'; } ?>">
-            <?php while (have_posts()): the_post(); ?>
-                <?php get_template_part('template-parts/content', 'page'); ?>
-            <?php endwhile; ?>
-        </div>
+        <section class="section-p-t section-p-b bg-dark relative">
+            <div class="container mx-auto <?php if( $pagewidth == 'narrow' ) { echo ' max-w-4xl '; } elseif( $pagewidth == 'fullwidth' ) { echo 'max-w-lvw'; } ?>">
+                <?php while (have_posts()): the_post(); ?>
+                    <?php get_template_part('template-parts/content', 'page'); ?>
+                <?php endwhile; ?>
+            </div>
+        </section>
     <?php endif; ?>
 
 <?php
