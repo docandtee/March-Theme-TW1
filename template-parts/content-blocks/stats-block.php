6d0c3f96840ea339ad25a3e0217c98d932874a52
Damien Doherty <damien@docandtee.com>
2026-04-28 12:41:39 +0100

    stats

--- template-parts/content-blocks/stats-block.php
+++ template-parts/content-blocks/stats-block.php
@@ -80,7 +80,7 @@
 							
 						<?php if( have_rows('statistics') ): ?>
 							<div class="statistics">
-								<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+								<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
 									<?php
 										while( have_rows('statistics') ): the_row();
 										$stats_number = get_sub_field('stats_number');
