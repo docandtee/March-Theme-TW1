933a5458c6bdf5d84ac859c0d3ceea79cfdebe23
Damien Doherty <damien@docandtee.com>
2026-05-29 16:50:46 +0100

    columns block

--- template-parts/content-blocks/columns-block.php
+++ template-parts/content-blocks/columns-block.php
@@ -24,10 +24,10 @@
 			?>
 				<div class="col-span-12 md:col-span-6 <?php echo esc_attr($number_of_columns); ?> mb-4 md:mb-0 border-t-6 border-primary p-6
 					<?php 
-						if (in_array($background_colour, ['bg-light', 'bg-tertiary', 'bg-secondary'])) {
-							echo 'bg-dark';
-						} else {
+						if (in_array($background_colour, ['bg-dark'])) {
 							echo 'bg-secondary';
+						} else {
+							echo 'bg-dark';
 						}
 					?>
 				">
