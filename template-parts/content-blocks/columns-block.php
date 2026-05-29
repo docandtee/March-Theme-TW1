43d0fb59db4d389b4a39d5ffeff83ac1b063238a
Damien Doherty <damien@docandtee.com>
2026-05-29 16:18:45 +0100

    columns block

--- template-parts/content-blocks/columns-block.php
+++ template-parts/content-blocks/columns-block.php
@@ -22,7 +22,7 @@
 				$column_subtitle = get_sub_field('column_subtitle');
 				$column_copy = get_sub_field('column_copy');
 			?>
-				<div class="col-span-12 md:col-span-6 bg-secondary <?php echo esc_attr($number_of_columns); ?> mb-4 md:mb-0 border-t-6 border-primary p-4">
+				<div class="col-span-12 md:col-span-6 bg-secondary <?php echo esc_attr($number_of_columns); ?> mb-4 md:mb-0 border-t-6 border-primary p-6">
 					<?php if($column_number) {
 						echo '<div class="number text-primary font-bold text-4xl mb-0 block">' . esc_html($column_number) . '</div>';
 					} ?>
