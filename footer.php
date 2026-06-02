e76e736d7cb694a4d947cffa75cc20977b4903ff
Damien Doherty <damien@docandtee.com>
2026-06-02 12:34:36 +0100

    footer nav

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 
     <?php do_action('tailpress_content_after'); ?>
 
-    <footer id="colophon" class="bg-dark" role="contentinfo">
-        <div class="container mx-auto py-5 lg:py-10">
+    <footer id="colophon" class="bg-dark border-t-1 border-tertiary section-p-t section-p-b" role="contentinfo">
+        <div class="container mx-auto">
             <?php do_action('tailpress_footer'); ?>
             <div class="grid grid-flow-row grid-cols-12">
                 <?php dynamic_sidebar('sidebar-footer'); ?>
