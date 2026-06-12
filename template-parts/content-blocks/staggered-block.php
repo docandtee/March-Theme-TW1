add5d878b17b7f1cd486244085ef9a58e4cd9688
Damien Doherty <damien@docandtee.com>
2026-06-12 14:17:42 +0100

    staggered block

--- template-parts/content-blocks/staggered-block.php
+++ template-parts/content-blocks/staggered-block.php
@@ -21,7 +21,7 @@
 		
 		<?php if ( $image ): ?>
 			<div class="img-block col-span-12 lg:col-span-6">
-				<div class="relative h-full min-h-128">
+				<div class="relative lg:h-full lg:min-h-128">
 					<?php docandtee_responsive_image( $image, null, '(min-width: 960px) 50vw, 100vw', 'aspect-3/2 w-full absolute lg:h-full lg:object-cover' ); ?>
 				</div>
 			</div>
